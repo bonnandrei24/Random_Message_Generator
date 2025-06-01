c5f743a5b6d3b7d0d814dc556039296cc0e4d67d
bonnandrei24 <buenaventurabonnandrei@gmail.com>
2025-06-01 18:07:24 +0800

    Add files via upload

---
+++ random_message_app.py
@@ -0,0 +1,37 @@
+# %%
+import random
+import streamlit as st
+
+# List of quotes
+quotes = [
+    "What meant for you will always be yours, and it will find you in any way.",
+    "It has always been hard, but you have always been brave.",
+    "Don't forget, you matter, and you always will.",
+    "Be yourself; everyone else is already taken.",
+    "Embrace the beauty of uncertainty. It’s a place of infinite possibility.",
+    "Don't forget to be kind to yourself. You deserve the same love and care that you give to others.",
+    "You will overcome all the barriers you face. You will succeed.",
+    "Life finds a way to get you where you're meant to be",
+    "You are what you believe yourself to be. ",
+    "It is okay to be different, unique, and to be yourself.",
+    "The darkness in your mind doesn't erase the light that you carry in your heart.",
+    "The moon wears its scars in full view, and so can you — a testament that even broken light can be beautiful.",
+    "Wings are not built by standing still but by fighting against the winds; your struggles are teaching you to soar.",
+    "Leaves fall not because they are weak but because it is part of the dance with the seasons; so trust that even your losses are part of your strength.",
+
+]
+
+def get_random_quote():
+    return random.choice(quotes)
+
+# Streamlit app layout
+st.set_page_config(page_title="Random Mesesage", page_icon="💬")
+
+st.title("💬 Message for yourself")
+
+st.write("Click to get a message")
+
+if st.button("New Message"):
+    st.success(get_random_quote())
+
+
