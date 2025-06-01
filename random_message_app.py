eadd517a630531b64f0335b9c5d2e2f07244c01f
bonnandrei24 <buenaventurabonnandrei@gmail.com>
2025-06-01 22:52:54 +0800

    Update random_message_app.py

--- random_message_app.py
+++ random_message_app.py
@@ -27,6 +27,13 @@ quotes = [
     "Instead of worrying of what could go wrong, you should see the uncertainty as a sign that there's room for it to all work out."
     "Sabi nila, pag-inlove ka, tumitigil ang mundo mo.",
     "Always choose kindness",
+    "If life gets hard again, which it probably will at some point, I just want you to remember It’s okay to cry. It’s okay to slow down. It’s okay to admit that you’re tired.",
+    "You don’t have to have it all figured out or keep it all together all the time. Taking a moment to pause doesn’t mean you’re broken or failing. It means you’re a human—living, struggling, and still standing.",
+    "No matter what comes next, just keep going. You’ve made it this far—and that’s already something to be proud of.",
+    "Whatever struggles have defeated you, dreaming isn’t over. Whatever fault you may have done, it doesn't define you.",
+    "Now that you are doubting yourself and afraid of where you might end up—let me tell you, it's a matter of how you continue.",
+    "Pick yourself up and prove yourself wrong for doubting. You are your worst enemy—but you are also your greatest ally."
+    "Papasa ka  sa battery exams"
   
 ]
 
